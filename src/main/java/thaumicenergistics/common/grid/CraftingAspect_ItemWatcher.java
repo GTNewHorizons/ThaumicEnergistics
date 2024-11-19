9fa465cd8e60ed5b7314f2e9c1b67cac4915229a
RecursivePineapple <recursive_pineapple@proton.me>
2024-11-19 14:12:48 -0500

    Added cleanup for the fake essentia items
    The existing code will generate fake essentia items to finish crafting
    CPU jobs, but it never removes those items. Even if you remove all
    essentia from the ME system, these items will never be removed.
    
    This adds two mechanisms for removing these fake items. First, it
    removes the items when inserted into the ME system. Secondly, it will
    poll the ME system (it already had to do this to determine what essentia
    can be craft) whenever the essentia levels change and remove any fake
    items left over (e.g. from prior essentia crafting jobs).

--- src/main/java/thaumicenergistics/common/grid/CraftingAspect_ItemWatcher.java
+++ src/main/java/thaumicenergistics/common/grid/CraftingAspect_ItemWatcher.java
@@ -1,7 +1,9 @@
 package thaumicenergistics.common.grid;
 
+import appeng.api.config.Actionable;
 import appeng.api.networking.security.BaseActionSource;
 import appeng.api.networking.storage.IBaseMonitor;
+import appeng.api.storage.IMEMonitor;
 import appeng.api.storage.IMEMonitorHandlerReceiver;
 import appeng.api.storage.data.IAEItemStack;
 import thaumicenergistics.common.items.ItemCraftingAspect;
@@ -29,8 +31,15 @@ class CraftingAspect_ItemWatcher implements IMEMonitorHandlerReceiver<IAEItemSta
             final BaseActionSource actionSource) {
         for (IAEItemStack stack : change) {
             // Is the stack craftable, has NBT tag, and is a crafting aspect?
-            if (stack.isCraftable() && stack.hasTagCompound() && (stack.getItem() instanceof ItemCraftingAspect)) {
+            if (stack.hasTagCompound() && (stack.getItem() instanceof ItemCraftingAspect)) {
                 this.gridCache.markForUpdate();
+
+                // Remove any fake aspect stacks in the ME system immediately
+                // We'll also try to remove any that snuck in another way (legacy items, etc) when updating the essentia
+                // cache
+                if (monitor instanceof IMEMonitor<IAEItemStack>itemMonitor) {
+                    itemMonitor.extractItems(stack, Actionable.MODULATE, new BaseActionSource());
+                }
                 break;
             }
         }
