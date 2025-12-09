5c2da1c9c863bb1ad740b2750cdb52111867b09a
Kogepan229 <kogepan25629@gmail.com>
2025-12-09 22:36:18 +0900

    FIx migration owner of PartEssentiaTerminal

--- src/main/java/thaumicenergistics/common/parts/PartEssentiaTerminal.java
+++ src/main/java/thaumicenergistics/common/parts/PartEssentiaTerminal.java
@@ -1,6 +1,7 @@
 package thaumicenergistics.common.parts;
 
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
 
 import appeng.parts.reporting.PartTerminal;
 
@@ -14,5 +15,19 @@ public class PartEssentiaTerminal extends PartTerminal {
 
     public PartEssentiaTerminal(ItemStack is) {
         super(is);
+    }
+
+    private static final String NBT_KEY_OWNER = "Owner";
+
+    @Override
+    public void addToWorld() {
+        super.addToWorld();
+
+        // For back compatibility
+        NBTTagCompound data = this.getItemStack().getTagCompound();
+        if (data != null && data.hasKey(NBT_KEY_OWNER)) {
+            int ownerID = data.getInteger(NBT_KEY_OWNER);
+            this.getProxy().getNode().setPlayerID(ownerID);
+        }
     }
 }
