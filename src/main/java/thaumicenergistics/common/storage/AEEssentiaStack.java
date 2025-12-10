90d6b1827c1c2d27d3efad1c11385383dba00c52
Kogepan229 <kogepan25629@gmail.com>
2025-12-11 00:31:49 +0900

    Separate AEEssentiaStack renderer class

--- src/main/java/thaumicenergistics/common/storage/AEEssentiaStack.java
+++ src/main/java/thaumicenergistics/common/storage/AEEssentiaStack.java
@@ -12,12 +12,10 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
-import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.StatCollector;
 import net.minecraft.world.World;
 
 import org.jetbrains.annotations.NotNull;
-import org.lwjgl.opengl.GL11;
 
 import com.djgiannuzz.thaumcraftneiplugin.ModItems;
 import com.djgiannuzz.thaumcraftneiplugin.items.ItemAspect;
@@ -33,8 +31,8 @@ import cpw.mods.fml.relauncher.SideOnly;
 import io.netty.buffer.ByteBuf;
 import thaumcraft.api.aspects.Aspect;
 import thaumcraft.api.aspects.AspectList;
-import thaumcraft.client.lib.UtilsFX;
 import thaumcraft.common.Thaumcraft;
+import thaumicenergistics.client.render.RendererAEEssentiaStack;
 
 public class AEEssentiaStack extends AEStack<AEEssentiaStack> {
 
@@ -285,51 +283,14 @@ public class AEEssentiaStack extends AEStack<AEEssentiaStack> {
         return null;
     }
 
-    @SideOnly(Side.CLIENT)
-    private static final ResourceLocation UNKNOWN_TEXTURE = new ResourceLocation(
-            "thaumcraft",
-            "textures/aspects/_unknown.png");
-
     @Override
     @SideOnly(Side.CLIENT)
     public void drawInGui(Minecraft mc, int x, int y) {
-        if (this.hasPlayerDiscovered(mc.thePlayer)) {
-            // Draw the aspect
-            UtilsFX.drawTag(x, y, this.aspect, 0, 0, 0);
-        }
-        // Draw the question mark
-        else {
-            // Bind the Thaumcraft question mark texture
-            mc.renderEngine.bindTexture(UNKNOWN_TEXTURE);
-
-            int color = this.aspect.getColor();
-            GL11.glColor4ub(
-                    (byte) ((color >> 8) & 0xff),
-                    (byte) ((color >> 16) & 0xff),
-                    (byte) ((color >> 24) & 0xff),
-                    (byte) 255);
-
-            GL11.glEnable(GL11.GL_BLEND);
-            GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
-
-            // Ask Thaumcraft to draw the question texture
-            UtilsFX.drawTexturedQuadFull(x, y, 0);
-
-            // Disable blending
-            GL11.glDisable(GL11.GL_BLEND);
-        }
+        RendererAEEssentiaStack.drawInGui(this, mc, x, y);
     }
 
     @Override
     public void drawOnBlockFace(World world) {
-        GL11.glPushMatrix();
-
-        GL11.glTranslatef(0, -0.04F, 0);
-        GL11.glScalef(1.0f / 42.0f, 1.0f / 42.0f, 1.0f / 42.0f);
-        GL11.glTranslated(-8.0, -10.2, -10.6);
-
-        UtilsFX.drawTag(0, 0, this.aspect, 0, 0, 0);
-
-        GL11.glPopMatrix();
+        RendererAEEssentiaStack.drawOnBlockFace(this);
     }
 }
