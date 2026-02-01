b056536a0142fff7b4a7118a8a7ffe9f15afc403
Ranzu <66495944+Ranzuu@users.noreply.github.com>
2026-02-01 15:52:58 +0100

    Fixed Icon Size

--- src/main/java/thaumicenergistics/client/gui/buttons/GuiButtonCraftingStatus.java
+++ src/main/java/thaumicenergistics/client/gui/buttons/GuiButtonCraftingStatus.java
@@ -20,9 +20,6 @@ import thaumicenergistics.client.textures.AEStateIconsEnum;
 @SideOnly(Side.CLIENT)
 public class GuiButtonCraftingStatus extends ThEStateButton {
 
-    private static final int ICON_X_OFFSET = 4;
-    private static final int ICON_Y_OFFSET = 3;
-
     public GuiButtonCraftingStatus(final int ID, final int xPosition, final int yPosition, final int buttonWidth,
             final int buttonHeight) {
         super(ID, xPosition, yPosition, buttonWidth, buttonHeight, AEStateIconsEnum.CRAFT_ONLY, 0, 0, null);
@@ -39,13 +36,7 @@ public class GuiButtonCraftingStatus extends ThEStateButton {
                 AEStateIconsEnum.TAB_BUTTON_EDGELESS.getWidth(),
                 AEStateIconsEnum.TAB_BUTTON_EDGELESS.getHeight());
         if (this.stateIcon != null) {
-            this.drawIcon(
-                    minecraftInstance,
-                    this.stateIcon,
-                    this.xPosition + ICON_X_OFFSET,
-                    this.yPosition + ICON_Y_OFFSET,
-                    AEStateIconsEnum.STANDARD_ICON_SIZE,
-                    AEStateIconsEnum.STANDARD_ICON_SIZE);
+            this.drawIcon(minecraftInstance, this.stateIcon, this.xPosition, this.yPosition, this.width, this.height);
         }
     }
 
