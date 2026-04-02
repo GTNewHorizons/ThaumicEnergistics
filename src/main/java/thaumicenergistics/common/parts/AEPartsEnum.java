528a1073c4783a52ac6faf07adef105527224fe7
Ranzu <66495944+Ranzuu@users.noreply.github.com>
2026-04-02 17:24:21 +0200

    Register Upgrade Cards

--- src/main/java/thaumicenergistics/common/parts/AEPartsEnum.java
+++ src/main/java/thaumicenergistics/common/parts/AEPartsEnum.java
@@ -32,7 +32,10 @@ public enum AEPartsEnum {
     EssentiaLevelEmitter(ThEStrings.Part_EssentiaLevelEmitter, PartEssentiaLevelEmitter.class, true),
 
     EssentiaStorageBus(ThEStrings.Part_EssentiaStorageBus, PartEssentiaStorageBus.class, null,
-            generatePair(Upgrades.INVERTER, 1)),
+            generatePair(Upgrades.FUZZY, 1), generatePair(Upgrades.INVERTER, 1), generatePair(Upgrades.CAPACITY, 1),
+            generatePair(Upgrades.ORE_FILTER, 1), generatePair(Upgrades.STICKY, 1)
+
+    ),
 
     EssentiaExportBus(ThEStrings.Part_EssentiaExportBus, PartEssentiaExportBus.class,
             ThaumicEnergistics.MOD_ID + ".group.essentia.transport", generatePair(Upgrades.CAPACITY, 2),
