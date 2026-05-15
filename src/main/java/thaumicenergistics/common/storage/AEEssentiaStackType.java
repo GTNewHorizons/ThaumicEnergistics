f0a799aef9adc706b988003c1d6faf66b83c8425
lc-1337 <lordicocain@gmail.com>
2026-05-15 21:44:39 +0300

    adapt

--- src/main/java/thaumicenergistics/common/storage/AEEssentiaStackType.java
+++ src/main/java/thaumicenergistics/common/storage/AEEssentiaStackType.java
@@ -400,4 +400,9 @@ public class AEEssentiaStackType implements IAEStackType<AEEssentiaStack> {
     public AEEssentiaStack getTestStack() {
         return this.testStack.copy();
     }
+
+    @Override
+    public int getTypeWeight() {
+        return 2;
+    }
 }
